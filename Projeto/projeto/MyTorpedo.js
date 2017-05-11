8308098f4bb92ef11a8e923bff6c2ccfacc1b8e8
Catarina24 <aniratac.correia@gmail.com>
2017-05-12 00:27:27 +0100

    calculates bezier points, but code note working

--- Projeto/projeto/MyTorpedo.js
+++ Projeto/projeto/MyTorpedo.js
@@ -19,7 +19,15 @@
 
     this.visible = true;
 
-    this.animation = false;
+    this.rotateAnimation = false;
+    this.bezierAnimation = false;
+
+    this.animationTime;
+    this.P2;
+    this.P3;
+    this.P4;
+
+    this.timePassed = 0;
 
     this.metalAppearance = new CGFappearance(scene);
     this.metalAppearance.setAmbient(0.3, 0.3, 0.3, 1);
@@ -79,20 +87,27 @@ MyTorpedo.prototype.updateHorizontalPosition = function(posX, posZ){
     this.positionZ = posZ;
 }
 
+MyTorpedo.prototype.calculateVector = function(){
+
+    this.P4 = [this.scene.targets[0].positionX,
+               this.scene.targets[0].positionY,
+               this.scene.targets[0].positionZ];
+
+    var vector = [this.P4[0] - this.positionX,
+                  this.P4[1] - this.positionY,
+                  this.P4[2] - this.positionZ];
+
+    return vector;
+}
+
 MyTorpedo.prototype.calculateRotationAngle = function(){
 
-    var targetPos = [this.scene.targets[0].positionX,
-                     this.scene.targets[0].positionY,+
-                     this.scene.targets[0].positionZ];
-
     //calculate angle between vector and origin ("z")
-    var vector = [targetPos[0] - this.positionX,
-                  targetPos[2] - this.positionZ];
+    var vector = this.calculateVector();
 
     //cos(ang) = vector1 * vector2 / (normal(vector1) * normal(vector2));
-
-    var n1 = Math.sqrt(vector[0]*vector[0] + vector[1] * vector[1]);
-    var cos = vector[1] / n1;
+    var n1 = Math.sqrt(vector[0]*vector[0] + vector[2] * vector[2]);
+    var cos = vector[2] / n1;
     var angle = Math.acos(cos);
 
     if (vector[0] > 0)
@@ -102,18 +117,34 @@ MyTorpedo.prototype.calculateRotationAngle = function(){
 
 }
 
-MyTorpedo.prototype.animate = function(){
-   
-    //this.rotationAngle = this.calculateRotationAngle();
-    this.animation = true;
-    var distance;
+MyTorpedo.prototype.calculateDistance = function(){
+
+    var vector = this.calculateVector();
+
+    return Math.sqrt(vector[0]*vector[0] + vector[2] * vector[2]);
 
 }
 
 MyTorpedo.prototype.animate = function(){
-   
-    this.animation = true;
-    var distance;
+
+    this.rotateAnimation = true;
+
+    var vector = this.calculateVector();
+    var distance = this.calculateDistance();
+    this.animationTime = parseInt(distance);
+
+    //calculate bezier points P2 and P3
+    this.P2 = [
+        4/5 * vector[0]+this.positionX,
+        4/5 * vector[1]+this.positionY,
+        4/5 * vector[2]+this.positionZ
+    ];
+
+    this.P3 = [
+        this.scene.targets[0].positionX,
+        4/5 * this.positionY,
+        this.scene.targets[0].positionZ
+    ];         
 
 }
 
@@ -121,18 +152,40 @@ MyTorpedo.prototype.update = function(currTime){
 
     var dif = currTime - this.timePassed;
 
-    if (this.animation){
+    if (this.rotateAnimation){
 
         var finalAngle = this.calculateRotationAngle();
         
         if (this.rotationAngle >= finalAngle){
-            this.animation = false;
-            return;
+            this.rotateAnimation = false;
+            this.bezierAnimation = true;
+        }
+        else{
+            var angle = dif * finalAngle / 30;
+            this.rotationAngle += angle * Math.PI / 180;
+        }
+    }
+
+    /*
+    if(this.bezierAnimation){
+        
+        for (var t=0; t <= 1; t+=1/this.animationTime){
+
+            this.positionX = Math.pow((1-t), 3) * this.positionX + 3*Math.pow((1-t), 2) * t * this.P2[0] + 3*(1-t) * Math.pow(t, 2) * this.P3[0] + Math.pow(t, 3) * this.P4[0]; 
+            
+            console.log(this.positionX);
+
+            this.positionY = Math.pow((1-t), 3) * this.positionY + 3*Math.pow((1-t), 2) * t * this.P2[1] + 3*(1-t) * Math.pow(t, 2) * this.P3[1] + Math.pow(t, 3) * this.P4[1]; 
+
+            console.log(this.positionY);
+
+            this.positionZ = Math.pow((1-t), 3) * this.positionZ + 3*Math.pow((1-t), 2) * t * this.P2[2] + 3*(1-t) * Math.pow(t, 2) * this.P3[2] + Math.pow(t, 3) * this.P4[2]; 
+
+            console.log(this.positionZ);
         }
 
-        var angle = dif * finalAngle / 30;
-        this.rotationAngle += angle * Math.PI / 180;
-    }
+        this.bezierAnimation = false;
+    }*/
 
     this.timePassed = currTime;
 };
